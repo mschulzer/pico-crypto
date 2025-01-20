37432eb4f8938716347c66471c6eeac100d740c8
Morten Schultz <ms@brondby-gym.dk>
2025-01-20 11:13:13 +0100

    modified the examples

--- example.py
+++ example.py
@@ -1,10 +1,12 @@
-#from ufastrsa.genprime import genrsa
-from ufastrsa.rsa import RSA, genrsa
+from ufastrsa.genprime import genrsa
+from ufastrsa.rsa import RSA
 
 # Generate RSA key pair for Person A
 key_bits = 1024
-private_key_data = genrsa(key_bits)
-private_key = RSA(key_bits, **private_key_data)
+#_, n, e, d = genrsa(key_bits)
+#private_key = RSA(key_bits, n=n, e=e, d=d)
+
+private_key = RSA(*genrsa(key_bits, e=65537))
 
 # Serialize the public key to PEM format
 public_key_pem = private_key.public_key_to_pem()
