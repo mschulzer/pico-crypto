89a12bae4a80624aca5de2548a1a91316a9a743d
Morten Schultz <45948423+mschulzer@users.noreply.github.com>
2025-01-24 09:55:59 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,15 +1,12 @@
 from ufastrsa.genprime import genrsa
 from ufastrsa.rsa import RSA
 
-# Generate RSA key pair for Person A
+# Generér RSA-nøglepar til Person A
 key_bits = 1024
-#_, n, e, d = genrsa(key_bits)
-#private_key = RSA(key_bits, n=n, e=e, d=d)
-
 private_key = RSA(*genrsa(key_bits, e=65537))
 
-# Serialize the public key to PEM format
+# Konvertér den offentlige nøgle til PEM-format
 public_key_pem = private_key.public_key_to_pem()
 
-print("Person A's Public Key (PEM):")
+print("Person A's Offentlige nøgle (PEM):")
 print(public_key_pem)
